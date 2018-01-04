2ee5b28f17ea9b34647b16afd72a54b400958bd0
Joshua Rose <jrgiant_git@outlook.com>
2018-01-04 17:28:42 -0500

    license and readme added

--- functions.php
+++ functions.php
@@ -1,4 +1,17 @@
 <?php
+// Twentyseventeen Child Theme, adds Words over Parallax and Movement of Custom logo
+// Copyright (C) 2017  Joshua Rose
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
 // Exit if accessed directly
 if ( !defined( 'ABSPATH' ) ) exit;
 
@@ -11,7 +24,7 @@ if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
     }
 endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
-         
+
 if ( !function_exists( 'child_theme_configurator_css' ) ):
     function child_theme_configurator_css() {
         wp_enqueue_style( 'chld_thm_cfg_separate', trailingslashit( get_stylesheet_directory_uri() ) . 'ctc-style.css', array( 'chld_thm_cfg_parent','twentyseventeen-style' ));
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Twentyseventeen Child Theme
+### Adds options to the customizer-> Theme Options for the following changes
+- Writing Over the parallax images (including text, style, and font).
+
+### Adds Options to the Customizer-> Site Identity for the location of the Custom Logo.
+Options include:
+- Over the Header Image (i.e. original Position)
+- On the navigation bar (image shrinks to the navigation bar when nav is stuck to the top of the page).
+
+#### Contribution instructions
+Please use a pull request, submit an issue or contact me via github.
+
+##### Misc
+Child theme originally created through child theme configurator plugin. More information at [childthemeconfigurator.com](http://www.childthemeconfigurator.com/)
