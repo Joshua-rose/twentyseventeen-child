a98a18732d48a11ac3a07a103c234270d3d06ce6
Joshua Rose <jrgiant_git@outlook.com>
2018-07-03 17:20:38 -0400

    Adding basic functionality to the non front page

--- template-parts/header/header-image.php
+++ template-parts/header/header-image.php
@@ -12,7 +12,15 @@
 <div class="custom-header">
 
 		<div class="custom-header-media">
-			<?php the_custom_header_markup(); ?>
+		<?php if ( twentyseventeen_is_frontpage() ): 
+				the_custom_header_markup(); 
+				elseif (is_page() && get_theme_mod('alternative_page_header', false) && (get_theme_mod('alternative_page_header_media', 'nothing') !== 'nothing') ) :?>
+				<img src="<?php echo wp_get_attachment_image_src(get_theme_mod('alternative_page_header_media', 'nothing'), 'full')[0] ?>" alt="Testing" srcset="">
+					<script type="text/javascript">
+						console.log(`<?php var_dump(wp_get_attachment_image_src(get_theme_mod('alternative_page_header_media', 'nothing'), 'full'));?>`);
+					</script>
+				
+		<?php endif;?>
 		</div>
 
 	<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
